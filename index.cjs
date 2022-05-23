6d1c05742b05f4d0b522a5e3b454cacdb87f2712
Pretty <mphefopretty1997@gmail.com>
2022-05-23 22:34:06 +0200

    added functionality

--- index.cjs
+++ index.cjs
@@ -12,8 +12,15 @@ require('dotenv').config();
 // import Alpine from 'alpinejs'
 // window.Alpine = Alpine
 // Alpine.start()
+// const app = express();
 
 
+const jwt = require('jsonwebtoken')
+
+const garments = require('./garments.json');
+
+const pg = require('pg');
+const Pool = pg.Pool;
 
 const API = require('./api.cjs');
 const { default: axios } = require('axios');
@@ -22,11 +29,28 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const DATABASE_URL = process.env.DATABASE_URL;
+app.use(express.static('public'));
+
+const GarmentManager = require('./shop/garment-manager.cjs');
+const UserManager = require('./shop/user-manager.cjs');
+const ShoppingCart = require('./shop/shopping-cart.cjs');
+
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://missy_tee:missy123@localhost:5432/missy_tee_app';
 const pgp = PgPromise({});
 const db = pgp(DATABASE_URL);
+// const connectionString =
+//   process.env.DATABASE_URL ||
+//   'postgresql://missy_tee:missy123@localhost:5432/missy_tee_app';
+// const pool = new Pool({
+//   connectionString,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-app.get('/api/garments', function(req, res){
+// Routes
+app.get('/api/garments', async function(req, res){
 	const gender = req.query.gender;
 	const season = req.query.season;
 
@@ -45,6 +69,103 @@ app.get('/api/garments', function(req, res){
 	
 	res.json({ garments: filteredGarments });
 });
+app.get('/api/garments', async function(req, res){
+
+	const gender = req.query.gender;
+	const season = req.query.season;
+
+	const filteredGarments = await garmentManager.filter({
+		gender,
+		season
+	});
+
+	res.json({ 
+		garments : filteredGarments
+	});
+});
+
+app.post('/api/garments', authenticateToken, (req, res) => {
+
+	const {
+		description,
+		img,
+		gender,
+		season,
+		price
+	} = req.body;
+
+	if (!description || !img || !price) {
+		res.json({
+			status: 'error',
+			message: 'Please fill in the empty fields',
+		});
+	} else if (
+		garments.find((garment) =>
+		  _.isEqual(garment, {description, img, gender, season, price}),)) 
+		{
+		res.json({
+		  status: 'error',
+		  message: 'Garment already exists',
+		});
+	  }else {
+		garments.push({
+			description,
+			img,
+			gender,
+			season,
+			price
+		});
+
+		res.json({
+			status: 'success',
+			message: 'New garment added.',
+		});
+	}
+
+});
+
+app.get('/api/garments/price/:price', async function(req, res){
+	const maxPrice = Number(req.params.price);
+	const filteredGarments = garments.filter( garment => {
+
+		if (maxPrice > 0) {
+			return garment.price <= maxPrice;
+		}
+		return true;
+	});
+
+	res.json({ 
+		garments : filteredGarments
+	});
+});
+const generateAccessToken = (user) => {
+	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+	  expiresIn: '24h',
+	});
+  };
+  
+  app.post('/auth', (req, res) => {
+	const username = req.query.username;
+	if (username == 'matjutapretty') {
+	  const user = {username: 'matjutapretty'};
+	  const accessToken = generateAccessToken(user);
+
+	  res.json({accessToken: accessToken});
+	}
+	res.sendStatus(401);
+  });
+  
+  function authenticateToken(req, res, next) {
+	const token = req.query.token;
+	if (token == null) return res.sendStatus(401);
+  
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+	  if (err) return res.sendStatus(403);
+	  req.user = user;
+	  next();
+	});
+  }
+
 
 API(app, db);
 const PORT = process.env.PORT || 5001;
